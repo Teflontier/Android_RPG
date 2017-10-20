354073f0fa1e4b7cefbba5da1686cd97d94bc8cc
Jonsensens <lukas-weber-games@gmx.de>
2017-10-20 17:42:12 +0200

    Monster

--- Assets/Scripts/Mobs/Mob.cs
+++ Assets/Scripts/Mobs/Mob.cs
@@ -8,3 +8,19 @@ public class Mob : Entity {
         return true;
     }
 }
+//----------Ablauf Monster--------------------------------------------------------------------------------	
+		//Player in Angriffsreichweite ? 
+			//wenn ja gehe in ANGRIFF
+			//wenn nicht gehen in BEWEGUNG 
+		//---------------ANGRIFF---------------:	
+			// Wähle Helden der angegriffen wird (ausnahme skill zb Spott würde dies verhindern)
+			// würfle vorgesehene Würfel, werte diese aus
+			// wenn POWER gewürfelt wurde:
+				//Prüfe Debuffs des Helden (vergiftet,usw.)
+				//nutze Power für Debuffs von denen der Held noch nicht betroffen ist (siehe BUFFS DEBUFF ORDNER)	
+				//nutze Power um Schaden zu erhöhen
+		//--------------BEWEGUNG--------------:
+			//Prüfe welche Player nahe genug um ihn zu ereichen 
+				//wenn 2 oder mehr Player ereichbar sind prüfe wenigsten hp 
+				//laufe zu player mit geringsten HP (ausnahme skill zb Spott würde dies verhindern)
+				//Wenn kein Spieler ereichbar ???
