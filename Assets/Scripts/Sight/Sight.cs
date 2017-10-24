bb88ac6780c6cb43e4e19841241037251cd449d4
Schellner <Schelle-Jan@web.de>
2017-10-25 00:06:41 +0200

    Testing some Line of Sight stuff

--- Assets/Scripts/Sight/Sight.cs
+++ Assets/Scripts/Sight/Sight.cs
@@ -4,38 +4,71 @@ using UnityEngine;
 
 public class Sight : MonoBehaviour {
 
+    public float sightRadius = 10f;
+    public LayerMask layerMask;
     private MeshFilter meshFilter;
 
-    void Awake() {
+    public struct VertexPosWithAngle {
+        public Vector2 vertexPos;
+        public float angle;
+
+        public VertexPosWithAngle(Vector3 vertexPos, float angle) {
+            this.vertexPos = vertexPos;
+            this.angle = angle;
+        }
+    }
+
+    void Update() {
         meshFilter = GetComponent<MeshFilter>();
         Mesh mesh = new Mesh();
         meshFilter.mesh = mesh;
-        mesh.Clear();
 
-//        List<Vector2> viewPoints = new List<Vector2>();
-//        viewPoints.Add(new Vector2(-2, 2));
-//        viewPoints.Add(new Vector2(2, 2));
-        int viewPoints = 361;
-        int vertexCount = viewPoints + 1;
+        List<VertexPosWithAngle> viewPoints = new List<VertexPosWithAngle>();
+        Collider2D[] blockersInRange = Physics2D.OverlapCircleAll(transform.position, sightRadius, layerMask);
+        foreach (Collider2D col in blockersInRange) {
+            PolygonCollider2D polyCol = (PolygonCollider2D)col;
+            Vector2 polyPos = polyCol.transform.position;
+            foreach (Vector2 point in polyCol.points) {
+                Vector2 vertexPos = polyPos + point;
+                viewPoints.Add(new VertexPosWithAngle(vertexPos, Vector2.Angle(transform.position, vertexPos)));
+            }
+        }
+
+        viewPoints.Sort((v1, v2) => v1.angle.CompareTo(v2.angle));
+        List<Vector2> hitPoints = new List<Vector2>();
+        foreach (VertexPosWithAngle vertexPos in viewPoints) {
+            RaycastHit2D hit = Physics2D.Linecast(transform.position, vertexPos.vertexPos, layerMask);
+            if (hit.collider != null)
+                hitPoints.Add(hit.point);
+            else{
+                Vector2 direction = (vertexPos.vertexPos - (Vector2)transform.position).normalized;
+                hitPoints.Add((Vector2)transform.position + direction * sightRadius);
+            }
+        }
+
+        for (int i = 0; i < hitPoints.Count; i++)
+            Debug.DrawLine(transform.position, hitPoints[i]);
+
+        if (hitPoints.Count == 0)
+            return;
+
+        int vertexCount = hitPoints.Count + 1;
         Vector3[] vertices = new Vector3[vertexCount];
         int[] triangles = new int[(vertexCount - 2) * 3];
-        vertices[0] = Vector3.zero;
 
+        vertices[0] = Vector2.zero;
         for (int i = 0; i < vertexCount - 1; i++) {
-            Vector3 pos = Quaternion.AngleAxis(i, Vector3.back) * Vector3.up;
-            vertices[i + 1] = pos * 5;
+            vertices[i + 1] = transform.InverseTransformPoint(hitPoints[i]);
             if (i < vertexCount - 2) {
                 triangles[i * 3] = 0;
                 triangles[i * 3 + 1] = i + 1;
                 triangles[i * 3 + 2] = i + 2;
             }
         }
+
+        mesh.Clear();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
         mesh.RecalculateNormals();
-    }
-
-    void Update() {
-        
     }
 }
--- Assets/Shaders/Surface.shader
+++ Assets/Shaders/Surface.shader
@@ -3,6 +3,7 @@
 Shader "Custom/SurfaceShader" {
 	Properties {
         _MainTex ("Sprite Texture", 2D) = "white" {}
+        [Toggle] useSpriteRendererColor("use SpriteRenderer Color", Float) = 0
 	}
 	SubShader {
 		Stencil{
@@ -18,17 +19,20 @@ Shader "Custom/SurfaceShader" {
              #pragma multi_compile DUMMY PIXELSNAP_ON
   
              sampler2D _MainTex;
+             float useSpriteRendererColor;
  
              struct Vertex {
                  float4 vertex : POSITION;
                  float2 uv_MainTex : TEXCOORD0;
                  float2 uv2 : TEXCOORD1;
+                 float4 color : COLOR;
              };
      
              struct Fragment {
                  float4 vertex : POSITION;
                  float2 uv_MainTex : TEXCOORD0;
                  float2 uv2 : TEXCOORD1;
+                 float4 color : COLOR;
              };
 
              Fragment vert(Vertex v)
@@ -37,14 +41,18 @@ Shader "Custom/SurfaceShader" {
                  o.vertex = UnityObjectToClipPos(v.vertex);
                  o.uv_MainTex = v.uv_MainTex;
                  o.uv2 = v.uv2;
+                 o.color = v.color;
                  return o;
              }
 
              float4 frag(Fragment fragment) : COLOR
              {
                  float4 o = float4(1, 0, 0, 0.2);
-                 half4 c = tex2D (_MainTex, fragment.uv_MainTex);
-				 o.rgb = c.rgb;
+                 if(useSpriteRendererColor == 0){
+                 	half4 c = tex2D (_MainTex, fragment.uv_MainTex);
+				 	o.rgb = c.rgb;
+                 }else
+                 	o.rgb = fragment.color;
                  return o;
              }
  
